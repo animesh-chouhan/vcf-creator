80da285a48a6f026fa65c86049ff67f41532c545
Animesh Singh Chouhan <animeshsingh.iitkgp@gmail.com>
2021-07-05 22:25:34 +0530

    issue in cli text

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="vcf-creator",
-    version="1.0.8",
+    version="1.0.9",
     author="Animesh Singh Chouhan",
     author_email="animeshsingh.iitkgp@gmail.com",
     description="Generate vCard file from CSV",
